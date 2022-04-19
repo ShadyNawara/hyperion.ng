be129a703f61fd54abe0d1fb3fdf756f5758ed1e
Shady Nawara <shady.nawara@outlook.com>
2022-04-19 04:29:08 -0500

    change ledDevicefile to write binary data

--- libsrc/leddevice/dev_other/LedDeviceFile.cpp
+++ libsrc/leddevice/dev_other/LedDeviceFile.cpp
@@ -57,16 +57,8 @@ int LedDeviceFile::open()
 	if ( ! _file->isOpen() )
 	{
 		Debug(_log, "QIODevice::WriteOnly, %s", QSTRING_CSTR(_fileName));
-		if ( !_file->open(QIODevice::WriteOnly | QIODevice::Text) )
-		{
-			QString errortext = QString ("(%1) %2, file: (%3)").arg(_file->error()).arg(_file->errorString(),_fileName);
-			this->setInError( errortext );
-		}
-		else
-		{
-			_isDeviceReady = true;
-			retval = 0;
-		}
+		_isDeviceReady = true;
+		retval = 0;
 	}
 	return retval;
 }
@@ -91,33 +83,12 @@ int LedDeviceFile::close()
 
 int LedDeviceFile::write(const std::vector<ColorRgb> & ledValues)
 {
-	QTextStream out(_file);
-
-	//printLedValues (ledValues);
-
-	if ( _printTimeStamp )
-	{
-		QDateTime now = QDateTime::currentDateTime();
-		qint64 elapsedTimeMs = _lastWriteTime.msecsTo(now);
-
-		#if (QT_VERSION >= QT_VERSION_CHECK(5, 8, 0))
-			out << now.toString(Qt::ISODateWithMs) << " | +" << QString("%1").arg( elapsedTimeMs,4);
-		#else
-			out << now.toString(Qt::ISODate) << now.toString(".zzz") << " | +" << QString("%1").arg( elapsedTimeMs,4);
-		#endif
-	}
-
-	out << " [";
+	_file->open(QIODevice::WriteOnly);
 	for (const ColorRgb& color : ledValues)
-	{
-		out << color;
-	}
-
-	#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
-		out << QString("]") << Qt::endl;
-	#else
-		out << "]" << endl;
-	#endif
+    {
+        _file->write(reinterpret_cast<const char*>(&color), 3);
+    }
+	_file->close();
 
 	return 0;
 }
